701221c7d4b09bc39a605ea80948da5f802d66bf
berylhu43 <114014643+berylhu43@users.noreply.github.com>
2023-11-01 13:20:58 -0500

    Update .gitignore

---
+++ keys.R
@@ -0,0 +1,12 @@
+#twitter keys
+
+
+app = "reaction towards AI",
+consumer_key = "1vL7a9zDjaaYmEG2LiE2ydy42",
+consumer_secret = "ccIfo3nsAdTPmr0N8UFKylrWRlSQA8STcFReaBksBB5P0oEOZX",
+access_token = "1498499612679852036-XVT6tqoH2xaFoUukB1DWAkKqctl3d5",
+access_secret = "UUO91WjRfm8nYE2gr0yB38nqjTNa96TICepWVfZkeNHLQ"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAH8AqwEAAAAArOMNS1gMP5UBJnJAzPjdCjqHAYw%3Dy9p2dSnsEZPqaKZjTf3aSCbKL5nSQt36FldB9ocJedPbTNbfr9"
+
+client_id = "ZzdKRTU3ZlVHUktNZ0dEdjBMNDc6MTpjaQ"
+client_secret = "Cl5EI_8r-yz0k8l_bQlb8RV3G_Yv3nZbWy7syYBqKToIW9X9Fo"
